51de45bc67520c336f583ca9807bd8953d64cc08
Nephty <211260@umons.ac.be>
2022-02-16 20:03:02 +0100

    xml follow up

--- src/main/java/front/XML/ResourceBundleBridge.java
+++ src/main/java/front/XML/ResourceBundleBridge.java
@@ -1,24 +1,43 @@
 package front.XML;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.ResourceBundle;
 
 public class ResourceBundleBridge extends ResourceBundle {
     public XMLResolver resolver;
+    public ArrayList<String> keys;
 
     public ResourceBundleBridge(XMLResolver resolver) {
         this.resolver = resolver;
+        ArrayList<String> keys = new ArrayList<>();
+        for (XMLElement e : resolver.content) {
+            keys.add(e.name);
+        }
     }
 
     @Override
-    protected Object handleGetObject(String key) {  // TODO : use the XMLResolver to find all the color values and create the if statements (or switch)
-        if (key.equals("test")) return "TEST";
+    protected Object handleGetObject(String key) {
+        for (XMLElement e : resolver.content) {
+            if (key.equals(e.name)) return e.value;
+        }
         return null;
     }
 
     @Override
     public Enumeration<String> getKeys() {
-        return Collections.enumeration(keySet());
+        return Collections.enumeration(keys);
     }
+
+    public ArrayList<String> getXMLKeys() {
+        return keys;
+    }
+
+    public String get(String key) {
+        if (handleGetObject(key) == null) return null;
+        return handleGetObject(key).toString();
+    }
+
+
 }

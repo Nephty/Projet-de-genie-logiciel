9c393633480c3f698f1002696522b429152316ed
Arnaud Moreau <72945143+Nephty@users.noreply.github.com>
2022-02-21 23:28:13 +0100

    Revert "Merge first additions"

--- src/main/java/front/navigation/navigators/LanguageButtonNavigator.java
+++
@@ -1,7 +0,0 @@
-package front.navigation.navigators;
-
-import javafx.scene.input.MouseEvent;
-
-public interface LanguageButtonNavigator {
-    void handleLanguageButtonNavigation(MouseEvent event);
-}
--- src/main/java/front/scenes/Scenes.java
+++
@@ -1,7 +0,0 @@
-package front.scenes;
-
-import javafx.scene.Scene;
-
-public class Scenes {
-    public static Scene AuthScene, SignInScene, LanguageScene, SignUpScene, MainScreenScene;
-}

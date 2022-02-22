40083a437797321f68b0610a48cb53862fad716b
Arnaud Moreau <72945143+Nephty@users.noreply.github.com>
2022-02-22 08:28:43 +0100

    manually merging XML branch

---
+++ src/main/java/front/controllers/SignInSceneController.java
@@ -0,0 +1,73 @@
+package front.controllers;
+
+import front.Main;
+import front.navigation.Flow;
+import front.navigation.navigators.BackButtonNavigator;
+import front.navigation.navigators.LanguageButtonNavigator;
+import front.scenes.Scenes;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class SignInSceneController implements BackButtonNavigator, LanguageButtonNavigator {
+
+    @FXML
+    Button languageButton, backButton, signInButton;
+    @FXML
+    TextField usernameField;
+    @FXML
+    PasswordField passwordField;
+    @FXML
+    Label incorrectUsernameOrPasswordLabel;
+
+    String username = "admin", password = "sudo letmein";  // TODO : back-end : get the username and the password from the database in these variables
+
+    @FXML
+    public void handleLanguageButtonClicked(MouseEvent event) {
+        handleLanguageButtonNavigation(event);
+    }
+
+    @FXML
+    public void handleSignInButtonClicked(MouseEvent event) {
+        if (usernameField.getText().equals(username) && passwordField.getText().equals(password)) {
+            if (incorrectUsernameOrPasswordLabel.isVisible()) incorrectUsernameOrPasswordLabel.setVisible(false);
+            passwordField.setText("");
+            usernameField.setText("");
+            Main.setScene(Flow.forward(Scenes.MainScreenScene));
+        } else {
+            if (!incorrectUsernameOrPasswordLabel.isVisible()) incorrectUsernameOrPasswordLabel.setVisible(true);
+        }
+    }
+
+    @FXML
+    public void handleBackButtonClicked(MouseEvent event) {
+        handleBackButtonNavigation(event);
+    }
+
+    @Override
+    public void handleBackButtonNavigation(MouseEvent event) {
+        Main.setScene(Flow.back());
+    }
+
+    @Override
+    public void handleLanguageButtonNavigation(MouseEvent event) {
+        Main.setScene(Flow.forward(Scenes.LanguageScene));
+    }
+
+    public void handleUsernameFieldKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) triggerSignIn();
+    }
+
+    public void handlePasswordFieldKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) triggerSignIn();
+    }
+
+    public void triggerSignIn() {
+        handleSignInButtonClicked(null);
+    }
+}
---
+++ src/main/java/front/scenes/Scenes.java
@@ -0,0 +1,7 @@
+package front.scenes;
+
+import javafx.scene.Scene;
+
+public class Scenes {
+    public static Scene AuthScene, SignInScene, LanguageScene, SignUpScene, MainScreenScene;
+}

aacfce1351d9f7172f41ad33bac53339c35c02c4
Nephty <arnaud.moreau@outlook.com>
2022-02-23 21:28:04 +0100

    added requests scene, modified a few attributes name to better reflect their usage, added @FXML tags where they were missing

--- src/main/java/front/animation/threads/FadeOutThread.java
+++ src/main/java/front/animation/threads/FadeOutThread.java
@@ -4,10 +4,22 @@ import front.animation.FadeOutTransition;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
 
+/**
+ * This class is used to make use of a fade out transition. It creates a new <code>Thread</code> and uses it
+ * to properly fade out the given <code>Node</code>, while the main thread can keep going on its own, independently of
+ * the fade out transition.
+ */
 public class FadeOutThread extends Thread {
     int sleepDuration, fadeInDuration, fadeOutDuration;
     Label label;
 
+    /**
+     * Sets the sleep duration, fade
+     * @param fadeInDuration
+     * @param fadeOutDuration
+     * @param sleepDuration
+     * @param label
+     */
     public void customStart(int fadeInDuration, int fadeOutDuration, int sleepDuration, Label label) {
         this.fadeInDuration = fadeInDuration;
         this.fadeOutDuration = fadeOutDuration;
---
+++ src/main/java/front/controllers/RequestsSceneController.java
@@ -0,0 +1,2 @@
+package front.controllers;public class RequestsSceneController {
+}

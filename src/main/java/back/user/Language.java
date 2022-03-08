7372817deac6cd2e2ecc3fec8b9893eb3bff0b2d
Nephty <arnaud.moreau@outlook.com>
2022-03-08 22:00:47 +0100

    changed the language scene layout to match the overall theme

---
+++ src/main/java/back/user/Language.java
@@ -0,0 +1,14 @@
+package back.user;
+
+public class Language {
+    public String name;
+
+    public Language(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- src/main/java/front/controllers/LanguageSceneController.java
+++ src/main/java/front/controllers/LanguageSceneController.java
@@ -1,21 +1,26 @@
 package front.controllers;
 
 import BenkyngApp.Main;
+import back.user.Language;
 import front.navigation.Flow;
 import front.navigation.navigators.BackButtonNavigator;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 
 public class LanguageSceneController extends Controller implements BackButtonNavigator {
-
+    @FXML
+    public ListView<Language> languagesListView;
     @FXML
     Button backButton, addButton, setButton;
     @FXML
     Label chooseLanguageLabel;
 
     public void initialize() {
+        languagesListView.setItems(FXCollections.observableArrayList(new Language("language A"), new Language("language B")));
         // TODO : back-end : fetch languages
     }
 
@@ -30,6 +35,11 @@ public class LanguageSceneController extends Controller implements BackButtonNav
     }
 
     @FXML
-    public void handleAddButtonClicked(MouseEvent mouseEvent) {
+    public void handleAddButtonMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    public void handleSetButtonMouseClicked(MouseEvent event) {
+
     }
 }

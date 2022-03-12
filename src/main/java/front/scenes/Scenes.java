500f0cec5b89e0ddfafd4dc9aedbad7119d631f4
Frix <210809@umons.ac.be>
2022-03-12 14:05:09 +0100

    Modifs de Nephty

--- src/main/java/front/scenes/Scenes.java
+++ src/main/java/front/scenes/Scenes.java
@@ -10,5 +10,4 @@ public class Scenes {
             NotificationsScene, RequestsScene, RequestsStatusScene, RequestNewPortfolioScene,
             RequestTransferPermissionScene, FinancialProductsScene, ProductDetailsScene, TransactionsHistoryScene,
             VisualizeToolScene, ExportHistoryScene, TransferScene, EnterPINScene;
-    // TODO : don't do a "toggle product scene", but add a button "toggle" and a label "status : " and a label "toggled/untoggled" instead
 }

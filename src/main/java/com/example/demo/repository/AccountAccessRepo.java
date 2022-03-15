1c502f6c19f77b14d60d00fa3a7be8666a2b9779
LicorneRose <liproca46578410>
2022-03-16 00:55:40 +0100

    AccountAccessRepo delete problem
    Using custom delete query because we had a weird sql error with the spring query.

--- src/main/java/com/example/demo/repository/AccountAccessRepo.java
+++ src/main/java/com/example/demo/repository/AccountAccessRepo.java
@@ -7,13 +7,22 @@ import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+@Transactional
 public interface AccountAccessRepo extends JpaRepository<AccountAccess, AccountAccessPK> {
 
     @Query("SELECT s FROM AccountAccess s, User u where u.userID = ?1 and s.userId = u.userID " +
             "order by s.accountId.swift.swift")
     List<AccountAccess> getAllByUserId(String userID);
+
+    @Modifying
+    @Query("DELETE FROM AccountAccess a WHERE a.accountId.iban = ?1 AND a.userId.userID = ?2")
+    void deleteAccountAccessByAccountIdAndUserId(String accountId, String userId);
 }
+

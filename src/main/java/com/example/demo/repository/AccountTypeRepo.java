5978ff7f08b284061a6c4d75f5541ec82f6ccb66
LicorneRose <liproca46578410>
2022-03-17 00:34:15 +0100

    Testing Repo :
    - UserRepo : Deleted all custom queries, no test.
    - AccountRepo : Deleted all custom queries, no test.
    - AccountAccessRepo : GetAllByUserId & deleteAccess
    more will come tomorrow
    
    AccountType : forgot the column annualFee

--- src/main/java/com/example/demo/repository/AccountTypeRepo.java
+++ src/main/java/com/example/demo/repository/AccountTypeRepo.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface AccountTypeRepo extends JpaRepository<AccountType, Integer> {
 }
--- src/main/java/com/example/demo/repository/CurrencyTypeRepo.java
+++ src/main/java/com/example/demo/repository/CurrencyTypeRepo.java
@@ -4,10 +4,11 @@ import com.example.demo.model.CurrencyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface CurrencyTypeRepo extends JpaRepository<CurrencyType,Integer> {
 
-    Optional<CurrencyType> findCurrencyTypeByCurrencyId(Integer id);
 }

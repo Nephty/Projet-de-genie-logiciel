926f0b6517b7d901a411bdcd08dda1893a5ec7ac
Nephty <211260@umons.ac.be>
2022-03-21 11:11:51 +0100

    Revert "adding languages"
    This reverts commit 3672a035c595d89ce1b1c222f216f13038e9f994.

--- src/main/java/front/scenes/SceneLoader.java
+++ src/main/java/front/scenes/SceneLoader.java
@@ -7,8 +7,6 @@ import javafx.scene.Scene;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 /**
  * Handles the loading of FXML files to create the Scenes.
@@ -18,16 +16,14 @@ public class SceneLoader {
     /**
      * Takes the given scene name to find the corresponding FXML file.
      *
-     * @param sceneName the name of the scene to load (that is, the name of the FXML file,
+     * @param sceneName - <code>String</code> - the name of the scene to load (that is, the name of the FXML file,
      *                  .fxml extension is not mandatory in the string, but it is in the name of the file
-     * @param locale the locale representing the correct language, for example en_US, fr_BE...
-     * @return the Scene that was created from the FXML file
+     * @return <code>Scene</code> - the Scene that was created from the FXML file
      */
-    public static Scene load(String sceneName, Locale locale) {
+    public static Scene load(String sceneName) {
         if (!sceneName.endsWith(".fxml")) sceneName += ".fxml";
         URL url = Main.class.getResource("/xml/scenes/" + sceneName);
-        ResourceBundle bundle = ResourceBundle.getBundle("lang.UIData", locale);
-        FXMLLoader FXMLLoader = new FXMLLoader(url, bundle);
+        FXMLLoader FXMLLoader = new FXMLLoader(url);
         try {
             Parent sceneParent = FXMLLoader.load();
             return new Scene(sceneParent, 1280, 720);

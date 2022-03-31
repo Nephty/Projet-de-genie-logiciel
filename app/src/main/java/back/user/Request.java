419d9a55d5d9518866ed8f1b819d3438e1f22182
Nephty <211260@umons.ac.be>
2022-03-31 16:50:56 +0200

    adding transfer permission request

--- app/src/main/java/back/user/Request.java
+++ app/src/main/java/back/user/Request.java
@@ -1,21 +1,29 @@
 package back.user;
 
-import app.Main;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-
 public class Request extends Communication {
-    private CommunicationType communicationType;
-    private String recipientId;
+    private final CommunicationType communicationType;
 
-
-    public Request(String swift, CommunicationType communicationType) {
-        this.recipientId = swift;
+    /**
+     * Creates a request with all the required information
+     *
+     * @param client            The Profile object of a user
+     * @param bank              The Bank object of the destinated bank
+     * @param communicationType The communicationType of the request
+     */
+    public Request(Profile client, Bank bank, CommunicationType communicationType) {
+        this.client = client;
+        this.bank = bank;
         this.communicationType = communicationType;
         this.wallet = null;
     }
 
+    /**
+     * Creates a request with all the required information
+     *
+     * @param client            The Profile object of a user
+     * @param wallet            The Wallet object of the destinated bank
+     * @param communicationType The communicationType of the request
+     */
     public Request(Profile client, Wallet wallet, CommunicationType communicationType) {
         this.client = client;
         this.wallet = wallet;
@@ -24,25 +32,7 @@ public class Request extends Communication {
     }
 
     public void send() {
-        int comType = 6;
-        switch(this.communicationType.toString()){
-            case("CREATE_ACCOUNT"): comType = 0; break;
-            case("CREATE_SUB_ACCOUNT"): comType = 1; break;
-            case("TRANSFER_PERMISSION"): comType = 2; break;
-            case("NEW_PORTFOLIO"): comType = 3; break;
-            case("NEW_WALLET"): comType = 5; break;
-        }
-        Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = null;
-        try {
-            response = Unirest.post("https://flns-spring-test.herokuapp.com/api/notification")
-                    .header("Authorization", "Bearer "+ Main.getToken())
-                    .header("Content-Type", "application/json")
-                    .body("{\r\n    \"notificationType\": "+ comType +",\r\n    \"comments\": \" \",\r\n    \"status\": \"Unchecked\",\r\n    \"recipientId\": \""+this.recipientId+"\"\r\n}")
-                    .asString();
-        } catch (UnirestException e) {
-            e.printStackTrace();
-        }
+        // TODO : Send the request to the API
     }
 
     public CommunicationType getReason() {

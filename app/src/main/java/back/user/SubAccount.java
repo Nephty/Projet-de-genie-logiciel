a63ee31d9e6213bf57658b85e7321282cb6ce976
Frix <210809@umons.ac.be>
2022-04-11 12:03:39 +0200

    Safe delete Transaction

--- app/src/main/java/back/user/SubAccount.java
+++ app/src/main/java/back/user/SubAccount.java
@@ -12,7 +12,6 @@ public class SubAccount {
     private final String IBAN;
     private final Currencies currency;
     private final double amount;
-    private final ArrayList<Transaction> transactionHistory;
 
     /**
      * Creates a SubAccount object with an HTTP request by using the IBAN and the currency of an account
@@ -39,38 +38,6 @@ public class SubAccount {
         String body = response.getBody();
         JSONObject obj = new JSONObject(body);
         this.amount = obj.getDouble("currentBalance");
-
-
-        // Fetch all the transactions
-        Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response2 = null;
-        try {
-            response2 = Unirest.get("https://flns-spring-test.herokuapp.com/api/transaction?iban=" + IBAN + "&currencyId=0")
-                    .header("Authorization", "Bearer " + Main.getToken())
-                    .asString();
-        } catch (UnirestException e) {
-            e.printStackTrace();
-        }
-        Main.errorCheck(response2.getStatus());
-        String body2 = response2.getBody();
-        body2 = body2.substring(1, body2.length() - 1);
-        this.transactionHistory = new ArrayList<Transaction>();
-
-        // If there is at least one transaction, it creates the transactions objects
-        if (!body2.equals("")) {
-            ArrayList<String> parsed = Bank.JSONArrayParser(body2);
-            for (int i = 0; i < parsed.size(); i++) {
-                JSONObject obj2 = new JSONObject(parsed.get(i));
-                long ID = obj2.getLong("transactionTypeId");
-                String senderName = obj2.getString("senderName");
-                String senderIBAN = obj2.getString("senderIban");
-                String receiverName = obj2.getString("recipientName");
-                String receiverIBAN = obj2.getString("recipientIban");
-                double amount = obj2.getDouble("transactionAmount");
-                String sendingDate = obj2.getString("transactionDate");
-                this.transactionHistory.add(new Transaction(ID, senderName, senderIBAN, receiverName, receiverIBAN, amount, sendingDate, Currencies.EUR));
-            }
-        }
     }
 
     public Currencies getCurrency() {
@@ -79,9 +46,5 @@ public class SubAccount {
 
     public double getAmount() {
         return this.amount;
-    }
-
-    public ArrayList<Transaction> getTransactionHistory() {
-        return this.transactionHistory;
     }
 }

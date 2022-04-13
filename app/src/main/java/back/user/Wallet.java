2e75e966c393b1b61cfa112c560e47549bf80914
Frix <210809@umons.ac.be>
2022-04-13 15:26:56 +0200

    Frix on the fix

--- app/src/main/java/back/user/Wallet.java
+++ app/src/main/java/back/user/Wallet.java
@@ -22,54 +22,51 @@ public class Wallet {
     public Wallet(Profile accountUser){
         this.accountUser = accountUser;
         this.bank = Main.getBank();
-        this.accountList = new ArrayList<Account>();
 
-        // Fetch all client's account access
+        // Fetch all active client's account
         Unirest.setTimeouts(0, 0);
         HttpResponse<String> response = null;
+
+        Unirest.setTimeouts(0, 0);
         try {
-            response = Unirest.get("https://flns-spring-test.herokuapp.com/api/account-access/all?userId=" + this.accountUser.getNationalRegistrationNumber())
+            response = Unirest.get("https://flns-spring-test.herokuapp.com/api/account-access/all?userId=" + this.accountUser.getNationalRegistrationNumber() + "&hidden=false&deleted=false")
+                    .header("Authorization", "Bearer " + Main.getToken())
+                    .asString();
+            Main.errorCheck(response.getStatus());
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.accountList = createsAccountList(response.getBody());
+
+
+        // Fetch deleted account
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response2;
+        try {
+            response2 = Unirest.get("https://flns-spring-test.herokuapp.com/api/account-access/all?userId="+this.accountUser.getNationalRegistrationNumber()+"&hidden=false&deleted=true")
                     .header("Authorization", "Bearer " + Main.getToken())
                     .asString();
         } catch (UnirestException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        Main.errorCheck(response.getStatus());
 
-        String body = response.getBody();
-        body = body.substring(1, body.length() - 1);
+        this.accountList.addAll(createsAccountList(response2.getBody()));
 
-        ArrayList<String> bodyList = Bank.JSONArrayParser(body);
-        // Creates the accounts
-        for (int i = 0; i < bodyList.size(); i++) {
-            JSONObject obj = new JSONObject(bodyList.get(i));
-            String swift = obj.getString("accountId");
-            Profile owner = new Profile(obj.getJSONObject("account").getString("ownerFirstname"), obj.getJSONObject("account").getString("ownerLastname"), obj.getString("userId"));
-            // TODO : Réparer
-            // Profile coOwner = new Profile(obj.getJSONObject("userId").getString("firstname"), obj.getJSONObject("userId").getString("lastname"), obj.getJSONObject("userId").getString("userID"));
-            String iban = obj.getString("accountId");
-            int accountTypeId = obj.getJSONObject("account").getInt("accountTypeId");
-            AccountType accountType = null;
-            switch (accountTypeId) {
-                case 0:
-                    accountType = AccountType.COURANT;
-                    break;
-                case 1:
-                    accountType = AccountType.JEUNE;
-                    break;
-                case 2:
-                    accountType = AccountType.EPARGNE;
-                    break;
-                case 3:
-                    accountType = AccountType.TERME;
-                    break;
-            }
-            boolean activated = obj.getBoolean("access");
-            boolean archived = obj.getBoolean("hidden");
-            boolean canPay = obj.getJSONObject("account").getBoolean("payment");
-            // TODO : Remettre coOwner
-            accountList.add(new Account(owner, owner, bank, iban, accountType, activated, archived, canPay));
+
+        // Fetch hidden account
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response3;
+        try {
+            response3 = Unirest.get("https://flns-spring-test.herokuapp.com/api/account-access/all?userId="+this.accountUser.getNationalRegistrationNumber()+"&hidden=true&deleted=false")
+                    .header("Authorization", "Bearer " + Main.getToken())
+                    .asString();
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
         }
+
+        this.accountList.addAll(createsAccountList(response3.getBody()));
+
     }
 
     public Wallet(Profile accountUser, ArrayList<Account> accountList){
@@ -79,49 +76,58 @@ public class Wallet {
     }
 
     public void update() throws UnirestException {
-        this.accountList = new ArrayList<Account>();
+//        this.accountList = new ArrayList<Account>();
+//
+//        // Fetch all client's account access
+//        Unirest.setTimeouts(0, 0);
+//        HttpResponse<String> response = Unirest.get("https://flns-spring-test.herokuapp.com/api/account-access/all?userId=" + this.accountUser.getNationalRegistrationNumber())
+//                .header("Authorization", "Bearer " + Main.getToken())
+//                .asString();
+//        Main.errorCheck(response.getStatus());
+//
+//        String body = response.getBody();
 
-        // Fetch all client's account access
-        Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest.get("https://flns-spring-test.herokuapp.com/api/account-access/all?userId=" + this.accountUser.getNationalRegistrationNumber())
-                .header("Authorization", "Bearer " + Main.getToken())
-                .asString();
-        Main.errorCheck(response.getStatus());
+    }
 
-        String body = response.getBody();
+    public ArrayList<Account> createsAccountList(String body){
+        ArrayList<Account> accountListRep = new ArrayList<Account>();
+
         body = body.substring(1, body.length() - 1);
-
         ArrayList<String> bodyList = Bank.JSONArrayParser(body);
-        // Creates the accounts
-        for (int i = 0; i < bodyList.size(); i++) {
-            JSONObject obj = new JSONObject(bodyList.get(i));
-            String swift = obj.getString("accountId");
-            Profile owner = new Profile(obj.getJSONObject("account").getString("ownerFirstname"), obj.getJSONObject("account").getString("ownerLastname"), obj.getString("userId"));
-            // TODO : Réparer
-            // Profile coOwner = new Profile(obj.getJSONObject("userId").getString("firstname"), obj.getJSONObject("userId").getString("lastname"), obj.getJSONObject("userId").getString("userID"));
-            String iban = obj.getString("accountId");
-            int accountTypeId = obj.getJSONObject("account").getInt("accountTypeId");
-            AccountType accountType = null;
-            switch (accountTypeId) {
-                case 0:
-                    accountType = AccountType.COURANT;
-                    break;
-                case 1:
-                    accountType = AccountType.JEUNE;
-                    break;
-                case 2:
-                    accountType = AccountType.EPARGNE;
-                    break;
-                case 3:
-                    accountType = AccountType.TERME;
-                    break;
+        if(!body.equals("")) {
+            // Creates the accounts
+            for (int i = 0; i < bodyList.size(); i++) {
+                JSONObject obj = new JSONObject(bodyList.get(i));
+                String swift = obj.getString("accountId");
+                Profile owner = new Profile(obj.getJSONObject("account").getString("ownerFirstname"), obj.getJSONObject("account").getString("ownerLastname"), obj.getString("userId"));
+                // TODO : Réparer
+                // Profile coOwner = new Profile(obj.getJSONObject("userId").getString("firstname"), obj.getJSONObject("userId").getString("lastname"), obj.getJSONObject("userId").getString("userID"));
+                String iban = obj.getString("accountId");
+                int accountTypeId = obj.getJSONObject("account").getInt("accountTypeId");
+                AccountType accountType = null;
+                switch (accountTypeId) {
+                    case 1:
+                        accountType = AccountType.COURANT;
+                        break;
+                    case 2:
+                        accountType = AccountType.JEUNE;
+                        break;
+                    case 3:
+                        accountType = AccountType.EPARGNE;
+                        break;
+                    case 4:
+                        accountType = AccountType.TERME;
+                        break;
+                }
+                boolean activated = obj.getBoolean("access");
+                boolean archived = obj.getBoolean("hidden");
+                boolean canPay = obj.getJSONObject("account").getBoolean("payment");
+                // TODO : Remettre coOwner
+
+                accountListRep.add(new Account(owner, owner, bank, iban, accountType, activated, archived, canPay));
             }
-            boolean activated = obj.getBoolean("access");
-            boolean archived = obj.getBoolean("hidden");
-            boolean canPay = obj.getJSONObject("account").getBoolean("payment");
-            // TODO : Remettre coOwner
-            accountList.add(new Account(owner, owner, bank, iban, accountType, activated, archived, canPay));
         }
+        return accountListRep;
     }
 
     public ArrayList<Account> getAccountList() {

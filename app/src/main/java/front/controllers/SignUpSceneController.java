93240a3ac132315394dbba893a02e6e543c659bc
Frix <210809@umons.ac.be>
2022-04-18 16:35:25 +0200

    Fix sign up

--- app/src/main/java/front/controllers/SignUpSceneController.java
+++ app/src/main/java/front/controllers/SignUpSceneController.java
@@ -229,7 +229,7 @@ public class SignUpSceneController extends Controller implements BackButtonNavig
             try {
                 response = Unirest.post("https://flns-spring-test.herokuapp.com/api/bank")
                         .header("Content-Type", "application/json")
-                        .body("{\r\n    \"swift\": \"" + SWIFT + "\",\r\n    \"name\": \"" + name + "\",\r\n    \"login\": \"" + " " + "\",\r\n    \"password\": \"" + password + "\",\r\n    \"address\": \"" + city + "\",\r\n    \"country\": \"" + country + "\",\r\n    \"defaultCurrencyType\": 0\r\n}")
+                        .body("{\r\n    \"swift\": \"" + SWIFT + "\",\r\n    \"name\": \"" + name + "\",\r\n    \"password\": \"" + password + "\",\r\n    \"address\": \"" + city + "\",\r\n    \"country\": \"" + country + "\",\r\n    \"defaultCurrencyId\": 0\r\n}")
                         .asString();
                 Main.errorCheck(response.getStatus());
             } catch (UnirestException e) {

0ea1285a581eaa5d69ab3abd03a960331ec14275
LicorneRose <liproca46578410>
2022-04-19 12:01:11 +0200

    Model javadoc done, again. Hope I don't lose it this time...

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -11,12 +11,19 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.sql.Date;
-import java.time.*;
-import java.util.Optional;
+import java.time.LocalDateTime;
 
-@Data @Slf4j
-@AllArgsConstructor
+/**
+ * The model for the table users.
+ * Check the entity relationShip diagram in the documentation if you need more info about this table <br>
+ * Setters, Getters, NoArgsConstructor, AllArgsConstructor and ToString method are implemented by {@link lombok}
+ */
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
+@Slf4j
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -60,10 +67,16 @@ public class User {
     )
     private Date birthdate;
 
-    public User(String userID){
-        this.userId = userID;
-    }
-
+    /**
+     * Custom constructor for User
+     * @param userID User's od
+     * @param username User's username
+     * @param lastname User's lastname
+     * @param firstname User's firstName
+     * @param email User's email
+     * @param language User's preferred language
+     * @param birthdate User's birthdate
+     */
     public User(String userID, String username, String lastname,
                 String firstname, String email, String language,Date birthdate) {
         this.userId = userID;
@@ -75,6 +88,10 @@ public class User {
         this.birthdate = birthdate;
     }
 
+    /**
+     * Custom constructor for User with the custom req body.
+     * @param userReq Custom request body for creating an account.
+     */
     public User(UserReq userReq) {
         userId = userReq.getUserId();
         username = userReq.getUsername();
@@ -86,6 +103,10 @@ public class User {
         birthdate = userReq.getBirthdate();
     }
 
+    /**
+     * Changes the password and/or language of the user
+     * @param userReq Custom request body for creating/modifying an account.
+     */
     public void change(UserReq userReq) {
         if(userReq.getPassword() != null) {
             password = userReq.getPassword();
@@ -94,19 +115,30 @@ public class User {
             language = userReq.getLanguage();
         }
     }
+
+    /**
+     * Get the full name of a user (ex: Moreau Cyril)
+     * @return A String containing the full name of the user -> lastname firstname
+     */
     @JsonIgnore
     public String getFullName() {
         return lastname + " " + firstname;
     }
 
+    /**
+     * @return true if the user is above or equal to 18 yo
+     * @see #getAge()
+     */
     @JsonIgnore
     public boolean isAbove18() {
         return getAge() >= 18;
     }
 
+    /**
+     * @return The age of the user found with his birthdate.
+     */
     @JsonIgnore
     public int getAge() {
         return LocalDateTime.now().getYear() - birthdate.toLocalDate().getYear();
     }
-
 }
